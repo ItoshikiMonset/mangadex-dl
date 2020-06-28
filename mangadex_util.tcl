a19811950d52557c92519d8649956e2367ae65d2
q3cpma <q3cpma@posteo.net>
2020-06-28 19:28:15 +0200

    Add a (common) user agent to curl

--- mangadex_util.tcl
+++ mangadex_util.tcl
@@ -4,6 +4,8 @@ set scriptdir [file dirname [file dirname \
 source [file join $scriptdir util.tcl]
 
 
+set user_agent {Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0}
+
 # Produce a daiz approved name for a chapter
 proc chapter_format_name {serie_name chapter_info} {
 	dict assign $chapter_info
@@ -31,17 +33,20 @@ proc chapter_format_name {serie_name chapter_info} {
 
 # Trivial wrapper
 proc json_dl {url} {
-    json::json2dict [exec curl -sL $url]
+	global user_agent
+	json::json2dict [exec curl --user-agent $user_agent --silent --location $url]
 }
 
 # Download the pages of a chapters starting at page $first and ending at page
 # $last in the CWD
 proc chapter_dl {chapter_id {first 0} {last end}} {
+	global user_agent
 	set json [json_dl https://mangadex.org/api/chapter/$chapter_id]
 	dict assign $json
 
 	set urls [lmap x [lrange $page_array $first $last] \
 				  {string cat $server$hash/ $x}]
-	catch {exec -ignorestderr curl --location --remote-name-all \
-		--connect-timeout 5 --max-time 15 --retry 5 {*}$urls}
+	catch {exec -ignorestderr curl --user-agent $user_agent --location \
+			   --remote-name-all --connect-timeout 5 --max-time 15 --retry 5 \
+			   {*}$urls}
 }
